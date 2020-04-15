4aab553ba727b46b543061359f80480a77b55c19
MondaleFelix <mondalefelix@yahoo.com>
2020-04-14 17:31:31 -0700

    connected book and author models

--- books/models.py
+++ books/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Book(models.Model):
-	title = models.CharField(max_length=200)
-	num_pages = models.IntegerField(default=0)
-	date_published = models.DateField()
-
-	def __str__(self):
-		return self.title
-
 class Author(models.Model):
 	name = models.CharField(max_length=200)
 	birth_date = models.DateField()
 
 	def __str__(self):
 		return self.name
+		
+class Book(models.Model):
+	title = models.CharField(max_length=200)
+	num_pages = models.IntegerField(default=0)
+	date_published = models.DateField()
+	author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+
+	def __str__(self):
+		return self.title
