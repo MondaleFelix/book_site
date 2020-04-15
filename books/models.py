db5da4e67b3c2a381b61690485de00def99c2a71
MondaleFelix <mondalefelix@yahoo.com>
2020-04-14 18:30:22 -0700

    added Tag model

--- books/models.py
+++ books/models.py
@@ -7,12 +7,17 @@ class Author(models.Model):
 
 	def __str__(self):
 		return self.name
-		
+
+class Tag(models.Model):
+	name = models.CharField(max_length=100)
+	
 class Book(models.Model):
 	title = models.CharField(max_length=200)
 	num_pages = models.IntegerField(default=0)
 	date_published = models.DateField()
 	author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+	tags = models.ManyToManyField(Tag)
 
 	def __str__(self):
 		return self.title
+
